43a6dc822b6a8ecc932156994dbe2fed08ed9986
Muhammad Genta Ari Shandi <muhammadgenta14@gmail.com>
2019-08-19 11:01:56 +0700

    add potensi model

---
+++ src/app/potensi/potensi.model.ts
@@ -0,0 +1,10 @@
+export class Potensi {
+  constructor(
+    public nama: string,
+    public info: string,
+    public kontak: string,
+    public website: string,
+    public peta: string,
+    public banner: string
+  ) {}
+}
