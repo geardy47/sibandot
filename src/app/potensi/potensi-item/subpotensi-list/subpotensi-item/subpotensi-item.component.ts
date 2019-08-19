e171df84f7837de2d5eeb329c99b5aa9c6d15e54
Muhammad Genta Ari Shandi <muhammadgenta14@gmail.com>
2019-08-19 11:25:47 +0700

    membuat data subpotensi list dinamis

--- src/app/potensi/potensi-item/subpotensi-list/subpotensi-item/subpotensi-item.component.ts
+++ src/app/potensi/potensi-item/subpotensi-list/subpotensi-item/subpotensi-item.component.ts
@@ -1,3 +1,4 @@
+import { Potensi } from './../../../potensi.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./subpotensi-item.component.css']
 })
 export class SubpotensiItemComponent implements OnInit {
+  potensi: Potensi[] = [
+    new Potensi(
+      'SD Negeri 4 Kranji',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis euismod neque. In hac habitasse platea dictumst.',
+      '(0281) 987654',
+      'localhost:4200',
+      '',
+      '../../../../../assets/img/potensi-item/sdkranji4.jpg'
+    ),
+    new Potensi(
+      'SD Negeri 4 Kranji',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis euismod neque. In hac habitasse platea dictumst.',
+      '(0281) 987654',
+      'localhost:4200',
+      '',
+      '../../../../../assets/img/potensi-item/sdkranji4.jpg'
+    ),
+    new Potensi(
+      'SD Negeri 4 Kranji',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis euismod neque. In hac habitasse platea dictumst.',
+      '(0281) 987654',
+      'localhost:4200',
+      '',
+      '../../../../../assets/img/potensi-item/sdkranji4.jpg'
+    ),
+    new Potensi(
+      'SD Negeri 4 Kranji',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis euismod neque. In hac habitasse platea dictumst.',
+      '(0281) 987654',
+      'localhost:4200',
+      '',
+      '../../../../../assets/img/potensi-item/sdkranji4.jpg'
+    ),
+    new Potensi(
+      'SD Negeri 4 Kranji',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis euismod neque. In hac habitasse platea dictumst.',
+      '(0281) 987654',
+      'localhost:4200',
+      '',
+      '../../../../../assets/img/potensi-item/sdkranji4.jpg'
+    ),
+    new Potensi(
+      'SD Negeri 4 Kranji',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis euismod neque. In hac habitasse platea dictumst.',
+      '(0281) 987654',
+      'localhost:4200',
+      '',
+      '../../../../../assets/img/potensi-item/sdkranji4.jpg'
+    )
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
