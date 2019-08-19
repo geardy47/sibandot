c446ab233062c60d7930ae029477a933c11eb1b6
yankotinuj <yankotinu.23@gmail.com>
2019-08-19 14:31:47 +0700

    Tambah google maps di detail potensi

--- src/app/potensi/potensi-item/subpotensi-list/subpotensi-item/subpotensi-detail/subpotensi-detail.component.ts
+++ src/app/potensi/potensi-item/subpotensi-list/subpotensi-item/subpotensi-detail/subpotensi-detail.component.ts
@@ -6,7 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./subpotensi-detail.component.css']
 })
 export class SubpotensiDetailComponent implements OnInit {
+  latitude = -7.428007;
+  longitude = 109.236618;
+  zoom: number = 15;
 
+  onChooseLocation(event) {
+    this.latitude = event.coords.lat;
+    this.longitude = event.coords.lng;
+  }
   constructor() { }
 
   ngOnInit() {
