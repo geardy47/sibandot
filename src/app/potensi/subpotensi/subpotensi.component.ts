274058d5872639d78e908b9154e03cc3708dd683
Muhammad Genta Ari Shandi <muhammadgenta14@gmail.com>
2019-08-21 09:55:37 +0700

    add subpotensi komponen

---
+++ src/app/potensi/subpotensi/subpotensi.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subpotensi',
+  templateUrl: './subpotensi.component.html',
+  styleUrls: ['./subpotensi.component.css']
+})
+export class SubpotensiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
